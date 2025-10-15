78c3c6173240e29a6452514a0149654216f7975c
Pablo Souza <pablo@pinaculodigital.com.br>
2025-10-15 18:09:01 -0300

    chore: change rules to remove unecessary errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/require-await': 'warn',
       'prettier/prettier': [
         'off',
         {
