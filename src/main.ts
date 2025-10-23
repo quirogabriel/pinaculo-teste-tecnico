46a1573fda131fe70524b28a67c0b4cec9c403a4
Pablo Souza <pablo@pinaculodigital.com.br>
2025-10-23 15:34:56 -0300

    feat: add documentation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,36 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Teste Técnico API')
+    .setDescription('Documentação da API')
+    .setVersion('0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.getHttpAdapter().get('/openapi.json', (_req, res) => {
+    res.json(document);
+  });
+
+  app.use(
+    '/docs',
+    apiReference({
+      layout: 'modern',
+      theme: 'purple',
+      url: '/openapi.json',
+      darkMode: true,
+      hideModels: true,
+      hideDownloadButton: true,
+    }),
+  );
+
   app.use(cookieParser());
   app.use(helmet());
 
@@ -18,6 +45,7 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3003;
   await app.listen(port);
 
-  console.log(`📡 Aplicação executando na porta ${port}`);
+  console.log(`-> Aplicação executando na porta ${port}`);
+  console.log(`-> Documentação disponível em http://localhost:3003/docs`);
 }
 void bootstrap();
