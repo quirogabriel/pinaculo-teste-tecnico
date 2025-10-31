2469e1a6bfa5933b93ed1c8ccb03e0576b57af84
Pablo Souza <pablo@pinaculodigital.com.br>
2025-10-31 14:42:52 -0300

    chore: turn off rule to remove confusing error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,6 +34,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/require-await': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'off',
       'prettier/prettier': [
         'off',
         {
